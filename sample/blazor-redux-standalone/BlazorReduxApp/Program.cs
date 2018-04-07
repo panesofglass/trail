def1f5703302d9d43bc6eb1b02a2e54c8b2a4903
Ryan Riley <ryan@tachyus.com>
2018-04-07 15:44:58 -0500

    Add Trail.ReduxBlazor and sample

---
+++ sample/blazor-redux-standalone/BlazorReduxApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Rendering;
+using Microsoft.AspNetCore.Blazor.Browser.Services;
+using System;
+using BlazorRedux;
+using Library1;
+
+namespace BlazorApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new BrowserServiceProvider(configure =>
+            {
+                // Add any custom services here
+                configure.AddSingleton(new Store<MyModel, MyMsg>(Store.Reducer, new MyModel(0, null)));
+            });
+
+            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
+        }
+    }
+}
