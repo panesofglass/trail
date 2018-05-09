c6dce9e776a623300f02c988dec2631f0f74c469
Ryan Riley <ryan@tachyus.com>
2018-05-09 09:33:38 -0500

    Begin adding Blazor 0.3.0 feature support

--- sample/standalone/Library1/Counter.fs
+++ sample/standalone/Library1/Counter.fs
@@ -23,16 +23,27 @@ type Counter () =
             Dom.h1 [] [Dom.text "Counter"]
             Dom.p [] [
                 Dom.text "Current count: "
-                Dom.textf "%i" this.CurrentCount
-            ]
-            Dom.button [
-                    Attr.onclick(this.IncrementCount)
-                ] [
-                    Dom.text "Click me"
+                Dom.input [
+                    Attr.typ "number"
+                    Attr.valuef "%i" this.CurrentCount
+                    Attr.onchange(fun e -> this.CurrentCount <- int(unbox<string> e.Value))
+                    Attr.isDisabled this.IsSelected
                 ]
+                Dom.input [
+                    Attr.typ "checkbox"
+                    Attr.isChecked this.IsSelected
+                    Attr.onchange(fun e -> this.IsSelected <- unbox<bool> e.Value)
+                ]
+            ]
+            Dom.button [ Attr.onclick(this.IncrementCount) ] [ Dom.text "Click me" ]
+            Dom.button [ Attr.onclick(this.Select) ] [ Dom.text "Select" ]
         ]
     
     member val private CurrentCount : int = 0 with get, set
+    member val private IsSelected : bool = false with get, set
 
     member this.IncrementCount _ =
         this.CurrentCount <- Library1.Counter.incrementBy(1, this.CurrentCount)
+    
+    member this.Select _ =
+        this.IsSelected <- not this.IsSelected
