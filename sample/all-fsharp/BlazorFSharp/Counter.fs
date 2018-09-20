9d625b6a84d30dba6daa33f29012e60d69540f7d
Ryan Riley <ryan@tachyus.com>
2018-09-20 13:19:45 -0500

    Add all F# sample

---
+++ sample/all-fsharp/BlazorFSharp/Counter.fs
@@ -0,0 +1,43 @@
+﻿namespace BlazorApp1.Pages
+
+open Microsoft.AspNetCore.Blazor.Components
+open Microsoft.AspNetCore.Blazor.Layouts
+open BlazorApp1.Shared
+open Trail
+
+[<LayoutAttribute(typeof<MainLayout>)>]
+[<RouteAttribute("/counter")>]
+type Counter () =
+    inherit Trail.Component()
+
+    let mutable currentCount = 0
+
+    let mutable isSelected = false
+
+    let incrementCount _ =
+        currentCount <- Library1.Counter.incrementBy(1, currentCount)
+    
+    let select _ =
+        isSelected <- not isSelected
+
+    override this.Render() =
+        Dom.Fragment [
+            Dom.h1 [] [Dom.text "Counter"]
+            Dom.p [] [
+                Dom.text "Current count: "
+                Dom.input [
+                    Attr.typ "number"
+                    Attr.valuef "%i" currentCount
+                    Attr.onchange(fun e -> currentCount <- int(unbox<string> e.Value))
+                    Attr.isDisabled isSelected
+                ]
+                Dom.input [
+                    Attr.typ "checkbox"
+                    Attr.isChecked isSelected
+                    Attr.onchange(fun e -> isSelected <- unbox<bool> e.Value)
+                ]
+            ]
+            Dom.button [Attr.className "btn btn-primary"; Attr.onclick incrementCount] [Dom.text "Click me"]
+            Dom.button [Attr.className "btn btn-primary"; Attr.onclick select] [Dom.text "Select"]
+        ]
+    
